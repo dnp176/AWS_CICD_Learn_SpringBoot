1cee5ddad433a3b6eab29095347ed702366eeb83
DNP DEVELOPER <44169383+dnp176@users.noreply.github.com>
2025-06-02 19:50:17 +0530

    Update stop_server.sh new2

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "=== Stopping existing application (if running) ==="
-PID=$(ps -ef | grep 'SimpleAWSApp' | grep -v grep | awk '{print $2}')
 
-if [ -z "$PID" ]; then
-  echo "No existing application running."
+if [ -f /home/ubuntu/deployments/app.pid ]; then
+  PID=$(cat /home/ubuntu/deployments/app.pid)
+  if ps -p $PID > /dev/null; then
+    echo "Killing existing application process $PID"
+    kill -9 $PID
+  fi
+  rm -f /home/ubuntu/deployments/app.pid
 else
-  echo "Killing process ID: $PID"
-  kill -9 $PID
+  echo "No existing application running."
 fi
